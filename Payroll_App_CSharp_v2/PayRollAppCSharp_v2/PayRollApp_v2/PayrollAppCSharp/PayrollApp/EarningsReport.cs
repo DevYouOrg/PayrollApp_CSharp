e7663ead493fd3682ab9e3df23317f8862f02d01
DevYouOrg <59802697+DevYouOrg@users.noreply.github.com>
2020-08-10 17:11:07 -0400

    Final solution

---
+++ Payroll_App_CSharp_v2/PayRollAppCSharp_v2/PayRollApp_v2/PayrollAppCSharp/PayrollApp/EarningsReport.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace PayrollApp
+{
+    public class EarningsReport
+    {
+        public static void PrintEmployeeReport(string employeePath)
+        {
+            List<Employee> employeeList = FileReaderWriter.GetEmployeeList(employeePath);
+
+
+            foreach (var item in employeeList)
+            {
+                Console.WriteLine($"{item.Name} will earn {item.CalculateTotalPay():C} this pay period.");
+            }
+
+            double employeeTotalHoursWorked = CalculateEmployeeTotalHoursWorked(employeePath);
+
+            Console.WriteLine(Environment.NewLine);
+
+            Console.WriteLine($"The total hours worked by the employees is {employeeTotalHoursWorked}.");
+
+        }
+
+        public static void PrintManagerReport(string managerPath)
+        {
+            List<Manager> managerList = FileReaderWriter.GetManagerList(managerPath);
+
+
+            foreach (var item in managerList)
+            {
+                Console.WriteLine($"{item.Name} will earn {item.CalculateTotalPay():C} this pay period.");
+            }
+
+            double managerTotalHoursWorked = CalculateManagerTotalHoursWorked(managerPath);
+
+            Console.WriteLine(Environment.NewLine);
+
+            Console.WriteLine($"The total hours worked by the managers is {managerTotalHoursWorked}.");
+
+        }
+
+        public static void PrintContractorReport(string contractorPath)
+        {
+            List<Contractor> contractorList = FileReaderWriter.GetContractorList(contractorPath);
+
+            foreach (var item in contractorList)
+            {
+                Console.WriteLine($"{item.Name} will earn {item.CalculateTotalPay():C} this pay period.");
+            }
+
+            double contractorTotalHoursWorked = CalculateContractorTotalTotalHoursWorked(contractorPath);
+
+            Console.WriteLine(Environment.NewLine);
+
+            Console.WriteLine($"The total hours worked by the contractors is {contractorTotalHoursWorked}.");
+        }
+
+        public static double CalculateEmployeeTotalHoursWorked(string employeePath)
+        {
+            List<Employee> employeeList = FileReaderWriter.GetEmployeeList(employeePath);
+
+            double employeeSum = 0;
+            foreach (var entry in employeeList)
+            {
+                employeeSum += entry.HoursWorked;
+            }
+
+            return employeeSum;
+
+        }
+
+        public static double CalculateManagerTotalHoursWorked(string managerPath)
+        {
+            List<Manager> managerList = FileReaderWriter.GetManagerList(managerPath);
+
+
+            double managerSum = 0;
+            foreach (var entry in managerList)
+            {
+                managerSum += entry.HoursWorked;
+            }
+
+            return managerSum;
+
+        }
+
+        public static double CalculateContractorTotalTotalHoursWorked(string contractorPath)
+        {
+            List<Contractor> contractorList = FileReaderWriter.GetContractorList(contractorPath);
+
+
+            double contractorSum = 0;
+
+            foreach (var entry in contractorList)
+            {
+                contractorSum += entry.HoursWorked;
+            }
+
+            return contractorSum;
+
+        }
+
+    }
+}
---
+++ Payroll_App_CSharp_v2/PayRollAppCSharp_v2/PayRollApp_v2/PayrollAppCSharp/PayrollApp/FileReaderWriter.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PayrollApp
+{
+    public class FileReaderWriter
+    {
+
+        public static List<Employee> GetEmployeeList(string employeePath)
+        {
+
+            //read from text file
+            string[] lines = File.ReadAllLines(employeePath);
+
+
+
+
+            List<Employee> employeeList = new List<Employee>();
+
+
+            foreach (string line in lines)
+            {
+                if (!string.IsNullOrEmpty(line))
+                {
+                    Employee employee = new Employee();
+
+                    var staffArray = line.Split('|');
+                    employee.Name = staffArray[0];
+                    employee.HoursWorked = Convert.ToDouble(staffArray[1]);
+
+                    employeeList.Add(employee);
+                }
+            }
+
+            return employeeList;
+        }
+
+
+
+
+
+
+
+        public static List<Manager> GetManagerList(string managerPath)
+        {
+            string[] lines = File.ReadAllLines(managerPath);
+            List<Manager> managerList = new List<Manager>();
+
+
+            foreach (string line in lines)
+            {
+                if (!string.IsNullOrEmpty(line))
+                {
+                    Manager manager = new Manager();
+
+                    var staffArray = line.Split('|');
+                    manager.Name = staffArray[0];
+                    manager.HoursWorked = Convert.ToDouble(staffArray[1]);
+
+                    managerList.Add(manager);
+                }
+            }
+
+            return managerList;
+        }
+
+
+
+
+        public static List<Contractor> GetContractorList(string contractorPath)
+        {
+            string[] lines = File.ReadAllLines(contractorPath);
+            List<Contractor> contractEmployees = new List<Contractor>();
+
+
+            foreach (string line in lines)
+            {
+                if (!string.IsNullOrEmpty(line))
+                {
+                    Contractor contractor = new Contractor();
+
+                    var staffArray = line.Split('|');
+                    contractor.Name = staffArray[0];
+                    contractor.HoursWorked = Convert.ToDouble(staffArray[1]);
+
+                    contractEmployees.Add(contractor);
+                }
+            }
+
+            return contractEmployees;
+        }
+
+
+
+        public static void WriteStaffHoursToTextFile(string staffChoice, string staffName, string staffHours, string managerPath, string employeePath, string contractorPath)
+        {
+
+            string path = "";
+            if (staffChoice == "m")
+            {
+                path = managerPath;
+            }
+
+            if (staffChoice == "e")
+            {
+                path = employeePath;
+            }
+
+            if (staffChoice == "c")
+            {
+                path = contractorPath;
+            }
+
+
+            using (StreamWriter writer = File.AppendText(path))
+            {
+                writer.WriteLine($"{staffName}|{staffHours}");
+            }
+
+
+            Console.WriteLine($"Adding complete... {Environment.NewLine}");
+        }
+
+
+    }
+}
